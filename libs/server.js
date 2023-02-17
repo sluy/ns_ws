dc175a9b158cb0c280dc7134fa7262b94f294e09
Stefan Luy <sluy1283@gmail.com>
2023-02-17 11:12:23 -0400

    Example fix

--- libs/server.js
+++ libs/server.js
@@ -3,11 +3,7 @@ const app = express();
 const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
-const io = new Server(server, { cors: {
-    origin: '*',
-  }
-});
-
+const io = new Server(server, { cors: { origin: "*" } });
 
 io.on("connection", (socket) => {
   console.log("a user connected");
