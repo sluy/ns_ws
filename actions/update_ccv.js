d7db1731f5705307c5e5c93faec987f72ef8874d
Stefan Luy <sluy1283@gmail.com>
2023-02-20 15:14:12 -0400

    cliente_id added

--- actions/update_ccv.js
+++ actions/update_ccv.js
@@ -1,5 +1,8 @@
-const updateCCV = ({ io }) => {
-  io.emit("update-ccv", true);
+const updateCCV = ({ io, req }) => {
+  const clienteId = parseInt(req.query.cliente_id);
+  if (!isNaN(clienteId) && clienteId > 0) {
+    io.emit("update-ccv", clienteId);
+  }
 };
 
 module.exports = updateCCV;
